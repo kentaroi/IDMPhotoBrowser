274654ae15f52bda36dcd54afb8ab4de2f46f8ad
Andrey <andreybaskakov@mail.ru>
2020-11-10 21:53:12 +0300

    - background warning fixed

--- Classes/IDMPhoto.m
+++ Classes/IDMPhoto.m
@@ -147,10 +147,11 @@ caption = _caption;
 			
 			[[SDWebImageManager sharedManager] loadImageWithURL:_photoURL options:SDWebImageRetryFailed progress:^(NSInteger receivedSize, NSInteger expectedSize, NSURL * _Nullable targetURL) {
 				CGFloat progress = ((CGFloat)receivedSize)/((CGFloat)expectedSize);
-				
-				if (self.progressUpdateBlock) {
-					self.progressUpdateBlock(progress);
-				}
+				dispatch_async(dispatch_get_main_queue(), ^{
+					if (self.progressUpdateBlock) {
+						self.progressUpdateBlock(progress);
+					}
+				});
 			} completed:^(UIImage * _Nullable image, NSData * _Nullable data, NSError * _Nullable error, SDImageCacheType cacheType, BOOL finished, NSURL * _Nullable imageURL) {
 				if (image) {
 					self.underlyingImage = image;
